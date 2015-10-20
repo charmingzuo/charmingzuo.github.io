c58b0f44307ff0493272e86491f1699e746651e0
charmingzuo@gmail.com <charmingzuo@gmail.com>
2015-10-21 00:02:55 +0800

    NFW

--- test/res/js/index.jade.slides.js
+++ test/res/js/index.jade.slides.js
@@ -1,3 +1,10 @@
 $(function () {
-    $('#lawyer-slides').carousel()
+    var $slideEl = $('#lawyer-slides');
+    if ($slideEl[0] && $slideEl.is(':visible')) {
+
+        $slideEl.carousel({
+            interval: 4500
+        });
+
+    }
 });
